7a6b0a138fc1bebbd3fb8e278cb6dbc837855876
Petr Broz <petr.broz@autodesk.com>
2022-05-04 16:22:52 +0200

    Updated lambda to download inputs from URL.

--- glb-to-usdz/app.py
+++ glb-to-usdz/app.py
@@ -1,12 +1,24 @@
+import requests
 import subprocess
 
 def lambda_handler(event, context):
-    input_path = '/function/' + event['input']
-    output_path = '/function/' + event['output']
+    url = event['url']
+    input_path = '/tmp/input.glb'
+    output_path = '/tmp/output.usdz'
     try:
+        print('Downloading file')
+        r = requests.get(url, allow_redirects=True)
+        open(input_path, 'wb').write(r.content)
+        print('Converting file')
         subprocess.run(['usd_from_gltf', input_path, output_path], check=True, stdout=subprocess.PIPE)
         return {
             'status': 'success'
+        }
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return {
+            'status': 'failed',
+            'message': 'Download failed'
         }
     except subprocess.CalledProcessError as err:
         print(err)
